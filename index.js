7c511ab2151d2d387bc1aed33be956cb2ef37968
Luke Boyett <lboyett@gmail.com>
2022-12-08 20:52:14 +0100

    add depth property to tree and node

--- index.js
+++ index.js
@@ -10,14 +10,20 @@ function labelSquares() {
   return squares;
 }
 let squares = labelSquares();
-function colorSquares(squares, position) {
+function colorSquares(squares, position, depth) {
   squares.forEach((square) => {
     if (position.toString() == square.textContent) {
-      square.style.backgroundColor = "gray";
+      if (depth == 0) square.style.backgroundColor = "black";
+      if (depth == 1) square.style.backgroundColor = "blue";
+      if (depth == 2) square.style.backgroundColor = "green";
+      if (depth == 3) square.style.backgroundColor = "red";
+      if (depth == 4) square.style.backgroundColor = "purple";
+      if (depth == 5) square.style.backgroundColor = "gray";
+      if (depth == 6) square.style.backgroundColor = "brown";
+      if (depth == 7) square.style.backgroundColor = "yellow";
     }
   });
 }
-
 function checkPos(posSquares) {
   let nextSquares = [];
   posSquares.forEach(([x, y]) => {
@@ -28,10 +34,18 @@ function checkPos(posSquares) {
   return nextSquares;
 }
 
+let board = [];
+for (let i = 0; i < 8; i++) {
+  board[i] = [];
+  for (let j = 0; j < 8; j++) {
+    board[i][j] = [i, j];
+  }
+}
+
 class Node {
-  constructor(position) {
+  constructor(position, depth = 0) {
     this.position = position;
-    this.depth = null;
+    this.depth = depth;
     this.path1 = null;
     this.path2 = null;
     this.path3 = null;
@@ -40,17 +54,20 @@ class Node {
     this.path6 = null;
     this.path7 = null;
     this.path8 = null;
-    colorSquares(squares, position);
+    colorSquares(squares, position, depth);
   }
 }
 
 class Tree {
-  constructor([x, y]) {
-    this.node = new Node([x, y]);
-    this.depth = 0;
+  constructor([x, y], depth = 0) {
+    this.node = new Node([x, y], depth);
+    this.depth = depth;
   }
   buildTree([x, y]) {
-    if (this.depth > 1) {
+    if (board[x][y] === "X") return null;
+    board[x][y] = "X";
+
+    if (this.depth + 1 > 2) {
       return;
     }
     let possibleSquares = [
@@ -65,33 +82,43 @@ class Tree {
     ];
     let nextSquares = checkPos(possibleSquares);
 
+    let depth = this.depth + 1;
+    console.log(depth);
+
     let counter = 1;
     nextSquares.forEach(([x, y]) => {
       if (counter == 1) {
-        this.node.path1 = new Tree([x, y]);
-        this.node.path1.depth = this.depth + 1;
+        this.node.path1 = new Tree([x, y], depth);
+        this.node.path1.depth = depth;
         this.node.path1.buildTree([x, y]);
       } else if (counter == 2) {
-        this.node.path2 = new Tree([x, y]);
-        // this.node.path2.buildTree([x, y]);
+        this.node.path2 = new Tree([x, y], depth);
+        this.node.path1.depth = depth;
+        this.node.path2.buildTree([x, y]);
       } else if (counter == 3) {
-        this.node.path3 = new Tree([x, y]);
-        // this.node.path3.buildTree([x, y]);
+        this.node.path3 = new Tree([x, y], depth);
+        this.node.path1.depth = depth;
+        this.node.path3.buildTree([x, y]);
       } else if (counter == 4) {
-        this.node.path4 = new Tree([x, y]);
-        // this.node.path4.buildTree([x, y]);
+        this.node.path4 = new Tree([x, y], depth);
+        this.node.path1.depth = depth;
+        this.node.path4.buildTree([x, y]);
       } else if (counter == 5) {
-        this.node.path5 = new Tree([x, y]);
-        // this.node.path5.buildTree([x, y]);
+        this.node.path5 = new Tree([x, y], depth);
+        this.node.path1.depth = depth;
+        this.node.path5.buildTree([x, y]);
       } else if (counter == 6) {
-        this.node.path6 = new Tree([x, y]);
-        // this.node.path6.buildTree([x, y]);
+        this.node.path6 = new Tree([x, y], depth);
+        this.node.path1.depth = depth;
+        this.node.path6.buildTree([x, y]);
       } else if (counter == 7) {
-        this.node.path7 = new Tree([x, y]);
-        // this.node.path7.buildTree([x, y]);
+        this.node.path7 = new Tree([x, y], depth);
+        this.node.path1.depth = depth;
+        this.node.path7.buildTree([x, y]);
       } else if (counter == 8) {
-        this.node.path8 = new Tree([x, y]);
-        // this.node.path8.buildTree([x, y]);
+        this.node.path8 = new Tree([x, y], depth);
+        this.node.path1.depth = depth;
+        this.node.path8.buildTree([x, y]);
       }
       counter++;
     });
